b042a5977bb6dd1614da328500b6609ef033d3cd
Mike Gibson <mike_gibson@live.co.uk>
2019-09-26 19:49:03 +0100

    Change assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const { PHASE_EXPORT, PHASE_PRODUCTION_BUILD } = require('next/constants')
+
 const withSass = require("@zeit/next-sass");
 
-module.exports = withSass({
-  cssModules: true,
-});
+module.exports = (phase) => {
+  return withSass({
+    cssModules: true,
+    assetPrefix: (phase === PHASE_EXPORT || phase === PHASE_PRODUCTION_BUILD)
+      ? '/countdown'
+      : '',
+  });
+}
